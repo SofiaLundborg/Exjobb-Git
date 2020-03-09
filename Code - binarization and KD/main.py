de243a255a02b47918f1e8bfe0b5f9d4879b221e
Sofia <sofialundborg94@hotmail.com>
2020-03-09 14:30:23 +0100

    batch size 64

--- Code - binarization and KD/main.py
+++ Code - binarization and KD/main.py
@@ -18,8 +18,8 @@ def load_data():
     normalizing_std = [0.229, 0.224, 0.225]
 
     if torch.cuda.is_available():
-        batch_size_training = 4
-        batch_size_validation = 4
+        batch_size_training = 64
+        batch_size_validation = 64
     else:
         batch_size_training = 4
         batch_size_validation = 16
