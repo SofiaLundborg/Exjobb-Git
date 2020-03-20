3a877ae9f23fcbe4074ddcbfa5f608c9551ef1e5
Sofia <sofialundborg94@hotmail.com>
2020-03-20 13:20:10 +0100

    small fix

--- Code - binarization and KD/main.py
+++ Code - binarization and KD/main.py
@@ -158,7 +158,7 @@ def train_first_layers(start_layer, end_layer, student_net, teacher_net, train_l
 
 def lit_training(student_net, train_loader, validation_loader, max_epochs=120, teacher_net=None):
 
-    student_dict = torch.load('./Trained_Models/' + 'after_lit_student_lit_Xnor++_20200320' + '.pth',
+    student_dict = torch.load('./Trained_Models/' + 'after_lit_student_input_lit_Xnor++_20200320' + '.pth',
                               map_location=get_device())
     student_net.load_state_dict(student_dict)
 
