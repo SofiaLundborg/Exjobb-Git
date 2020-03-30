d4bbc596f012d88c37ab14de5c507fbdb22462be
Sofia <sofialundborg94@hotmail.com>
2020-03-30 09:01:57 +0200

    method c finetuning

--- Code - binarization and KD/main.py
+++ Code - binarization and KD/main.py
@@ -176,16 +176,16 @@ def training_a(student_net, teacher_net, train_loader, validation_loader):
 
 def finetuning(net, train_loader, validation_loader, max_epochs):
 
-    student_dict = torch.load('./Trained_Models/LIT_with_one_shortcut_distribution_scalingXnor++_20200329.pth',
+    student_dict = torch.load('./Trained_Models/method_c_one_shortcut_distribution_scalingXnor++_20200329.pth',
                               map_location=get_device())
     net.load_state_dict(student_dict)
 
     layers_to_train = ['layer1', 'layer2', 'layer3']
     set_layers_to_binarize(net, layers_to_train)
 
-    title_loss = 'method b - loss, ' + str(net.net_type)
-    title_accuracy = 'method b - accuracy, ' + str(net.net_type)
-    filename = 'method_b_one_shortcut_distribution_scaling_finetuning' + str(net.net_type)
+    title_loss = 'method c - loss, ' + str(net.net_type)
+    title_accuracy = 'method c - accuracy, ' + str(net.net_type)
+    filename = 'method_c_one_shortcut_distribution_scaling_finetuning' + str(net.net_type)
 
     criterion = torch.nn.CrossEntropyLoss()
     if torch.cuda.is_available():
