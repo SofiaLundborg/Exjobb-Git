3fdf2829d9ca6c0f6e094840cb385933fce49bcb
Sofia <sofialundborg94@hotmail.com>
2020-04-17 15:03:20 +0200

    pin memory

--- Code - binarization and KD/main.py
+++ Code - binarization and KD/main.py
@@ -57,10 +57,10 @@ def load_imageNet():
     print('validation set is loaded')
 
     train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size_training,
-                                               shuffle=True, num_workers=16)
+                                               shuffle=True, num_workers=16, pin_memory=True)
     print('train_loader finished')
     validation_loader = torch.utils.data.DataLoader(validation_set, batch_size=batch_size_validation,
-                                                    shuffle=False, num_workers=16)
+                                                    shuffle=False, num_workers=16, pin_memory=True)
     print('validation_loader finished')
 
 
