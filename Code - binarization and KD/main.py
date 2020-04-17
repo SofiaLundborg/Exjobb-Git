924ead252327cbfbbe259d3bbab05d1f60fa4ae8
Sofia <sofialundborg94@hotmail.com>
2020-04-17 09:29:16 +0200

    wrong transformation

--- Code - binarization and KD/main.py
+++ Code - binarization and KD/main.py
@@ -51,7 +51,7 @@ def load_imageNet():
             transforms.ToTensor(),
             transforms.Normalize(mean=normalizing_mean, std=normalizing_std)])
 
-    train_set = torchvision.datasets.ImageNet(root='./data', split='train', transform=preprocessing_train)
+    train_set = torchvision.datasets.ImageNet(root='./data', split='train', transform=transform_train)
     validation_set = torchvision.datasets.ImageNet(root='./data', split='valid', transform=preprocessing_valid)
 
     train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size_training,
