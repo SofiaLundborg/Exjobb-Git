854d2062042adf559a6e1006db21b7b93de7ce12
Sofia <sofialundborg94@hotmail.com>
2020-04-17 09:35:49 +0200

    correct preprocessing

--- Code - binarization and KD/main.py
+++ Code - binarization and KD/main.py
@@ -51,8 +51,8 @@ def load_imageNet():
             transforms.ToTensor(),
             transforms.Normalize(mean=normalizing_mean, std=normalizing_std)])
 
-    train_set = torchvision.datasets.ImageNet(root='./data', split='train', transform=transform_train)
-    validation_set = torchvision.datasets.ImageNet(root='./data', split='valid', transform=transform_test)
+    train_set = torchvision.datasets.ImageNet(root='./data', split='train', transform=preprocessing_train)
+    validation_set = torchvision.datasets.ImageNet(root='./data', split='valid', transform=preprocessing_valid)
 
     train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size_training,
                                                shuffle=True, num_workers=2)
