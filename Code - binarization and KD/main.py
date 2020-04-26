method a with correct binarization in shorcut (not full precision)

Code - binarization and KD/main.py
@@ -266,6 +266,7 @@ def training_a(student_net, teacher_net, train_loader, validation_loader, filena
     optimizer = optim.Adam(student_net.parameters(), lr=0.01, weight_decay=0)
 
     layers = ['layer1', 'layer2', 'layer3', 'layer4', 'all']
+    layers = ['layer1', 'layer2', 'layer3', 'layer4']
     max_epoch_layer = 30
     max_epochs = max_epoch_layer * 3 + 60
 
@@ -300,7 +301,7 @@ def training_a(student_net, teacher_net, train_loader, validation_loader, filena
             max_epoch_layer = 60
             criterion = torch.nn.CrossEntropyLoss()
         else:
-            max_epoch_layer = 30
+            max_epoch_layer = 6
             set_layers_to_binarize(student_net, layers[:layer_idx+1])
         if student_net.dataset == 'ImageNet':
             cut_network = 1 + 4 * (layer_idx +1)
@@ -328,12 +329,14 @@ def training_a(student_net, teacher_net, train_loader, validation_loader, filena
 
         print(layer + " is training")
         #epoch = -1
-        while (epoch < max_epoch_layer) and (lr >= 1e-6):
-            epoch += 1
+
+        for epoch in range(max_epoch_layer):
+        #while (epoch < max_epoch_layer) and (lr >= 1e-5):
+            #epoch += 1
             start_time_epoch = time.time()
 
-            #total_epoch = epoch + 30*layer_idx
-            total_epoch += 1
+            total_epoch = epoch + 6*layer_idx
+            #total_epoch += 1
 
             if layer == 'all':
                 criterion = torch.nn.CrossEntropyLoss()
@@ -343,25 +346,29 @@ def training_a(student_net, teacher_net, train_loader, validation_loader, filena
             else:
                 set_layers_to_update(student_net, layers[:layer_idx+1])
 
-            learning_rate_change = [15, 20, 25]
+            learning_rate_change = [2, 4, 5]
             #learning_rate_change = [30, 45, 50, 55]
             if layer == 'all':
                 learning_rate_change = [50, 70, 90, 100]
                 learning_rate_change = [30, 40, 50]
 
-            if n_not_improved >= 2:
-                if layer == 'all':
-                    if n_change_learning_rate >= 4:
-                        lr = lr * 0.1
-                        n_change_learning_rate = 0
-                    else:
-                        n_change_learning_rate += 1
-                else:
-                    lr = lr * 0.1
-                for param_group in optimizer.param_groups:
-                    param_group['lr'] = lr
-                    n_not_improved = 0
+            # if n_not_improved >= 2:
+            #     if layer == 'all':
+            #         if n_change_learning_rate >= 4:
+            #             lr = lr * 0.1
+            #             n_change_learning_rate = 0
+            #         else:
+            #             n_change_learning_rate += 1
+            #     else:
+            #         lr = lr * 0.1
+            #     for param_group in optimizer.param_groups:
+            #         param_group['lr'] = lr
+            #         n_not_improved = 0
 
+        if epoch in learning_rate_change:
+            lr = lr * 0.1
+            for param_group in optimizer.param_groups:
+                param_group['lr'] = lr
                 print('learning rate decreased to: ' + str(lr))
 
             running_loss = 0
@@ -474,7 +481,6 @@ def training_a(student_net, teacher_net, train_loader, validation_loader, filena
                 best_validation_loss = validation_loss_for_epoch
                 best_epoch = total_epoch
 
-
             else:
                 n_not_improved += 1
 
@@ -495,7 +501,6 @@ def training_a(student_net, teacher_net, train_loader, validation_loader, filena
             log.write("TOTAL TIME for epoch " + str(total_epoch) + ": " + str(end_time_epoch-start_time_epoch) + " seconds\n\n\r" )
             log.close()
 
-            time.sleep(5)
         layer_idx += 1
         changed_layer = True
 
@@ -1192,7 +1197,7 @@ def main():
 
 
     filename = 'method_a_double_shortcut_with_finetuning_' + str(net_type)
-    student_ResNet18 = resNet.resnet_models['resnet18ReluDoubleShortcut'](net_type, 'ImageNet')
+    student_ResNet18 = resNet.resnet_models['resnet18ReluDoubleShortcut'](net_type, 'ImageNet', factorized_gamma=True)
     checkpoint_student = change_loaded_checkpoint(original_teacher_dict, student_ResNet18)
     student_ResNet18.load_state_dict(checkpoint_student)
     if torch.cuda.is_available():
@@ -1209,9 +1214,10 @@ def main():
     # epoch, student_ResNet18, optimizer, train_loss, validation_loss, train_accuracy, validation_accuracy, layer_index = load_training(
     #     student_ResNet18, optimizer, './saved_training/ImageNet/finetuning_after_method_a_double_shortcut_complete_setXnor++_20200424')
 
-    finetuning(student_ResNet18, train_loader, validation_loader, train_loader_not_disturbed, 30, filename=filename, saved_model='./saved_training/ImageNet/method_a_double_shortcut_with_relu_long_Xnor++_20200421')
+    #finetuning(student_ResNet18, train_loader, validation_loader, train_loader_not_disturbed, 30, filename=filename, saved_model='./saved_training/ImageNet/method_a_double_shortcut_with_relu_long_Xnor++_20200421')
 
-    #path = training_a(student_ResNet18, teacher_ResNet18, train_loader_subset, validation_loader_subset, filename, saved_training='./saved_training/ImageNet/method_a_double_shortcut_with_relu_long_Xnor++_20200421')
+    filename = 'method_a_correct_shortcut_factorized_Xnor++_'
+    path = training_a(student_ResNet18, teacher_ResNet18, train_loader_subset, validation_loader_subset, filename)
 
     print('finished training')
 

Code - binarization and KD/models/resNet.py
@@ -466,8 +466,10 @@ class BasicBlockAbs(nn.Module):
             if option == 'cifar10':
                 self.shortcut = LambdaLayer(lambda x: F.pad(x[:, :, ::2, ::2], (0, 0, 0, 0, planes//4, planes//4), "constant", 0))
             else:
+                input_size_temp = input_size
                 self.shortcut = nn.Sequential(
-                     nn.Conv2d(in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False),
+                     #nn.Conv2d(in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False),
+                     myConv2d(3, self.expansion*planes, input_size_temp, kernel_size=1, stride=stride, factorized_gamma=factorized_gamma),
                      nn.BatchNorm2d(self.expansion * planes)
                 )
 

Code - binarization and KD/plot_main.py
@@ -53,6 +53,15 @@ def binary_xnor_xnorpp():
     xnorppF_no_valid_loss = torch.load(
         'Results/validation_loss_no_method_double_shortcut_with_relu_factorized_Xnor++.pt')
 
+    print('binary a max: ' + str(max(binary_a_valid_acc)))
+    print('binary no method max: ' + str(max(binary_no_valid_acc)))
+    print('xnor a max: ' + str(max(xnor_a_valid_acc)))
+    print('xnor no method: ' + str(max(xnor_no_valid_acc)))
+    print('xnorpp a max: ' + str(max(xnorpp_a_valid_acc)))
+    print('xnorpp no method: ' + str(max(xnorpp_no_valid_acc)))
+    print('xnorppF a max: ' + str(max(xnorppF_a_valid_acc)))
+    print('xnorppF no method: ' + str(max(xnorppF_no_valid_acc)))
+
     max_epochs_a = 90
     max_epoch_finetuning = 110
 
@@ -461,7 +470,6 @@ def plot_traing_a(train_loss, valid_loss, train_acc, valid_acc, filename, suptit
         axs[0, 1].set_visible(False)
         axs[0, 2].set_visible(False)
 
-
         ax0.plot(epochs_training, train_loss[:max_epoch_training], linestyle='dashed', label='train')
         ax0.plot(epochs_training, valid_loss[:max_epoch_training], linestyle='solid', label='validation')
         ax0.set_xlabel('Epochs')
@@ -512,6 +520,53 @@ def single_double_relu():
     relu_double_valid_acc = torch.load('Results/validation_accuracy_method_a_double_shortcut_with_relu_Xnor++_20200410.pt')
     relu_double_train_loss = torch.load('Results/train_loss_method_a_double_shortcut_with_relu_Xnor++_20200410.pt')
     relu_double_valid_loss = torch.load('Results/validation_loss_method_a_double_shortcut_with_relu_Xnor++_20200410.pt')
+
+    max_epoch_training = 90
+    max_epoch_finetuning = 60
+    epochs_training = np.arange(max_epoch_training)
+    epochs_finetuning = np.arange(max_epoch_finetuning)
+
+    sns.set()
+    sns.set_style("whitegrid")
+    # sns.set_context("talk")
+    with sns.color_palette("bright"):
+        fig, axs = plt.subplots(2, 3, figsize=(9, 5.5))
+        ax0 = axs[1, 0]
+        ax1 = axs[1, 1]
+        ax2 = axs[1, 2]
+
+        axs[0, 0].set_visible(False)
+        axs[0, 1].set_visible(False)
+        axs[0, 2].set_visible(False)
+
+        ax0.plot(epochs_training, train_loss[:max_epoch_training], linestyle='dashed', label='train')
+        ax0.plot(epochs_training, valid_loss[:max_epoch_training], linestyle='solid', label='validation')
+        ax0.set_xlabel('Epochs')
+        ax0.set_ylabel('MSE loss')
+        ax0.set_title('Training using method a)')
+        ax0.legend(loc='upper left')
+
+        ax1.plot(epochs_finetuning, train_loss[max_epoch_training:], linestyle='dashed', label='train')
+        ax1.plot(epochs_finetuning, valid_loss[max_epoch_training:], linestyle='solid', label='validation')
+        ax1.set_xlabel('Epochs')
+        ax1.set_ylabel('Cross entropy loss')
+        ax1.set_title('Training after method a) - loss')
+        ax1.legend(loc='upper right')
+
+        ax2.plot(epochs_finetuning, train_acc, linestyle='dashed', label='train')
+        ax2.plot(epochs_finetuning, valid_acc, linestyle='solid', label='validation')
+        ax2.set_xlabel('Epochs')
+        ax2.set_ylabel('Cross entropy loss')
+        ax2.set_title('Training after method a) - accuracy')
+        ax2.legend(loc='lower right')
+
+        plt.suptitle(suptitle, fontsize=14, y=0.6)
+
+        plt.tight_layout()
+        fig.savefig(filename + '.eps', format='eps')
+
+        plt.show()
+
 
     plot_traing_a(naive_train_loss, naive_valid_loss, naive_train_acc, naive_valid_acc, 'naive_method_a', 'Training with method a) using naive ResNet block')
     plot_traing_a(relu_single_train_loss, relu_single_valid_loss, relu_single_train_acc, relu_single_valid_acc, 'relu_one_shortcut_a', 'Training with method a) using single ReLU shortcut')
@@ -613,6 +668,7 @@ def method_a_ImageNet():
         plt.show()
 
 def main():
+    binary_xnor_xnorpp()
     #single_double_relu()
     method_a_ImageNet()
 