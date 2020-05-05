b703ec7e1bdfbdc211715414bc221db3fff6c565
Sofia <sofialundborg94@hotmail.com>
2020-05-05 17:19:17 +0200

    small fix

--- Code - binarization and KD/models/resNet.py
+++ Code - binarization and KD/models/resNet.py
@@ -712,7 +712,7 @@ class BasicBlockBiReal(nn.Module):
     expansion = 1
 
     def __init__(self, in_planes, planes, input_size, stride=1, option='cifar10', net_type='full_precision', factorized_gamma=False):
-        super(BasicBlockNaiveDoubleShortcut, self).__init__()
+        super(BasicBlockBiReal, self).__init__()
         input_size_temp = input_size.copy()
         self.conv1 = my_conv3x3(in_planes, planes, input_size, net_type=net_type, stride=stride, bias=False, factorized_gamma=factorized_gamma)
         self.bn1 = nn.BatchNorm2d(planes)
