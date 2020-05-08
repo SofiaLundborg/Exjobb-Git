01bd4d3af75b7876843bc1d60c84a845f20597d5
Sofia <sofialundborg94@hotmail.com>
2020-05-08 20:27:52 +0200

    small fix

--- Code - binarization and KD/main.py
+++ Code - binarization and KD/main.py
@@ -11,13 +11,6 @@ def main():
 
     train_loader, validation_loader, test_loader, train_loader_not_augmented = load_cifar10(test_as_validation=True)
 
-    student_ResNet18 = resNet.resnet_models['resnet18Naive'](net_type=net_type, dataset='cifar10',
-                                                                          factorized_gamma=True)
-    if torch.cuda.is_available():
-        student_ResNet18 = student_ResNet18.cuda()
-    filename = 'xnor_double_shortcut_not_binarized_first_layer_bin_conv_in_shortcut'
-    finetuning(student_ResNet18, train_loader, validation_loader, train_loader_not_augmented, 120,
-               learning_rate_change=[70, 90, 100, 110], filename=filename)
 
     student_ResNet18 = resNet.resnet_models['resnet18Naive'](net_type=net_type, dataset='cifar10',
                                                              factorized_gamma=True)
